e86b8941e0539152f1d5dde34a5e5398fcc03424
EvgenijaAntonova20 <alexocean.e.k@gmail.com>
2023-11-27 11:23:18 +0600

    feat: header.js and readme

--- js/header.js
+++ js/header.js
@@ -4,16 +4,22 @@ header.style.transitionTimingFunction = 'ease';
 header.style.transitionProperty = 'top';
 
 let lastScrollY = 0;
-if (screen.width <= 992) {
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > lastScrollY) {
-            header.style.top = '-200px';
+const onWindowScroll = () => {
+    if (window.scrollY > lastScrollY) {
+        header.style.top = '-200px';
+    } else {
+        header.style.top = '0';
+        if (window.pageYOffset > 100) {
+            header.style.backgroundColor = 'rgba(191,25,104,0.8)';
         } else {
-            header.style.top = '0';
-            if (window.pageYOffset > 100) {
-                header.style.backgroundColor = 'rgba(191,25,104,0.8)'
-            }
+            header.style.backgroundColor = 'transparent';
         }
-        lastScrollY = window.scrollY;
-    });
+    }
+    lastScrollY = window.scrollY;
+}
+
+if (screen.width <= 992) {
+    window.addEventListener('scroll', onWindowScroll);
+} else {
+    window.removeEventListener('scroll', onWindowScroll);
 }
